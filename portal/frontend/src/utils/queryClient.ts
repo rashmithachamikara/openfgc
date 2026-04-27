33541a4c4d0ea42d1051fbed08c10b350c5a0ded
Rashmitha Chamikara <rashmitha.chamickara@gmail.com>
2026-04-27 19:12:37 +0530

    Reduce consent details cache TTL

--- portal/frontend/src/utils/queryClient.ts
+++ portal/frontend/src/utils/queryClient.ts
@@ -18,12 +18,12 @@
 
 import { QueryClient } from '@tanstack/react-query'
 
-const FIVE_MINUTES_IN_MS = 5 * 60 * 1000
+const STALE_TIME_IN_MS = 1 * 60 * 1000
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: FIVE_MINUTES_IN_MS,
+      staleTime: STALE_TIME_IN_MS,
       retry: 1,
       refetchOnWindowFocus: false,
     },
